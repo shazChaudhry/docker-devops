62b208ad7c551517a7107925f873bb68da8c1b93
Shahzad A. Chaudhry <shahzad_chaudhry@hotmail.com>
2018-12-21 23:33:01 +0000

    Updated terraform scripts

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,6 @@
 variable "public_key_path" {
   description = "Enter the path to the SSH Public Key to add to AWS."
-  default     = "/home/root/.ssh/id_rsa.pub"
+  default     = "/root/.ssh/id_rsa.pub"
 }
 
 variable "region" {
@@ -9,7 +9,7 @@ variable "region" {
 }
 
 variable "aws_credentials" {
-  default = "/home/root/.aws/credentials"
+  default = "/root/.aws/credentials"
 }
 
 variable "key_name" {
