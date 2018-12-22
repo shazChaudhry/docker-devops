bfa70d680473bc5feb0513d668c16da35a3c0d63
Shahzad A. Chaudhry <shahzad_chaudhry@hotmail.com>
2018-12-22 01:57:08 +0000

    experimenting

--- terraform/main.tf
+++ terraform/main.tf
@@ -52,7 +52,7 @@ terraform {
   # It is expected that the bucket already exists
   backend "s3" {
     bucket  = "ci.terraform"
-    key     = "dev/terraform.tfstate"
+    key     = "docker-devops/terraform.tfstate"
     region  = "eu-west-2"
     encrypt = true
   }
